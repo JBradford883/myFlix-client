f09c0677019c77132aa194132210ead1a48f41c6
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-20 00:23:52 -0400

    Removed placeholder logic and updated with logic to render external API correctly

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -12,20 +12,57 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [],
+      selectedMovie: null,
+      user: null,
+      register: false
     };
   }
 
-  setSelectedMovie(newSelectedMovie) {
+  componentDidMount() {
+    axios.get('https://myflix-2388-app.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  /* When a movie is clicked, this function is invoked and updates the state of the `selectMovie` *property to that movie */
+
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie
+    });
+  }
+
+  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user */
+
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
+  onRegister(register) {
+    this.setState({
+      register
     });
   }
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user, register } = this.state;
 
-    // If there are no movies in the list, return "The list is empty"
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the details are *passed as a prop to the LoginView */
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    /* Register user */
+    if (register) return <RegistrationView onRegister={(register) => this.onRegister(register)} />
+
+    // Before the movies have been loaded
+    if (movies.length === 0) return <div className="main-view" />;
 
     // If there are movies selected show the selected movie content
     return (
@@ -33,7 +70,7 @@ export class MainView extends React.Component {
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
           ))
         }
       </div>
