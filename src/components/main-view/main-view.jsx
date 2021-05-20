6b636dd88a43e502c380fdbff722720eab1bf874
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-20 00:07:24 -0400

    Removed placeholder array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,12 +11,7 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', Director: 'Christopher Nolan', Genre: 'Action', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg' },
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', Director: 'Frank Darabont', Genre: 'Drama', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg' },
-        { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', Director: 'Ridley Scott', Genre: 'Action', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Gladiator_%282000_film_poster%29.png' }
-      ],
-      selectedMovies: null
+      movies: [],
     };
   }
 
