29058287943d3c29fa0d552a3e454fb858eb88b9
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-21 13:08:50 -0400

    Removed old commented out code

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -28,20 +28,3 @@ export function LoginView(props) {
     </Form>
   );
 }
-
-/*
-return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUserName(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
-  );
-}
-*/
