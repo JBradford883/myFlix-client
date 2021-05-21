72b89261bb5ddc4b05c14321265058d781c6ff8a
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-21 13:06:38 -0400

    Updated form to use bootstrap styles

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -13,6 +13,24 @@ export function LoginView(props) {
   };
 
   return (
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="text" onChange={e => setUserName(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+
+      <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+    </Form>
+  );
+}
+
+/*
+return (
     <form>
       <label>
         Username:
@@ -26,3 +44,4 @@ export function LoginView(props) {
     </form>
   );
 }
+*/
