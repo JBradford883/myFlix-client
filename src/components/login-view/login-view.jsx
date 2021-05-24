83302623fda6f94e01dde569d4cbe20b8e206e21
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-24 13:06:35 -0400

    Added comment for return that gives username and password

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -14,6 +14,7 @@ export function LoginView(props) {
     props.onLoggedIn(username);
   };
 
+  // Login form for username and password
   return (
     <Form>
       <Form.Group controlId="formUsername">
