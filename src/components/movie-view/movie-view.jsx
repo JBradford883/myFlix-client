b83a9786f7789d51c02aebb2d994e6dcecf7d8b1
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-24 13:10:54 -0400

    Bolded the headlines

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -11,23 +11,23 @@ export class MovieView extends React.Component {
 
     return (
       <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
+        <div>
+          <img className="movie-img float-left" src={movie.ImagePath} />
         </div>
         <div className="movie-title">
-          <span className="label">Title: </span>
+          <span className="label"><b>Title:</b> </span>
           <span className="value">{movie.Title}</span>
         </div>
         <div className="movie-description">
-          <span className="label">Description: </span>
+          <span className="label"><b>Description:</b> </span>
           <span className="value">{movie.Description}</span>
         </div>
         <div className="movie-director">
-          <span className="label">Director: </span>
+          <span className="label"><b>Director:</b> </span>
           <span className="value">{movie.Director.Name}</span>
         </div>
         <div className="movie-genre">
-          <span className="label">Genre: </span>
+          <span className="label"><b>Genre:</b> </span>
           <span className="value">{movie.Genre.Name}</span>
         </div>
         <Button onClick={() => onBackClick(null)} variant="primary">Back</Button>
