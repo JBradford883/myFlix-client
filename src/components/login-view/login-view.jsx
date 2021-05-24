f0324b4ab50178edcb5667f2abc28f9997e1cb80
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-24 13:07:40 -0400

    Updated form to fit different screen sizes

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -16,20 +16,18 @@ export function LoginView(props) {
 
   // Login form for username and password
   return (
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUserName(e.target.value)} />
-      </Form.Group>
     <>
       <h1 className="headline text-center font-weight-bold text-danger">Welcome to myFlix App</h1>
+      <Form className="col-md-6 offset-3">
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
           <Form.Control type="text" placeholder="Enter Username" onChange={e => setUserName(e.target.value)} />
+        </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
           <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
 
         <Button variant="danger" type="submit" onClick={handleSubmit}>Submit</Button>
       </Form>
