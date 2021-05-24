96e34600f41e29741bc17755f4cb4141eb4c8cd1
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-24 13:12:53 -0400

    Removed comment

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { MainView } from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
 
-// Import statements to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
 //Main component  (will eventually use all the others)
