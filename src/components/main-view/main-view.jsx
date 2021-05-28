667ae644ac321e61acefa9eb1ba31ebf7264e999
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-28 11:51:13 -0400

    Imported ProfileView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,6 +9,7 @@ import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
+import { ProfileView } from '../profile-view/profile-view';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
