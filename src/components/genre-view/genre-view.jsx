793bec64521b158347220d895696f0829ea9c338
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-28 11:35:49 -0400

    Created Genre card

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export class GenreView extends React.Component {
+
+  render() {
+    const { genre, onBackClick } = this.props;
+
+    return (
+      <Card className='genre-view'>
+        <Card.Body>
+          <Card.Title><h1>{genre.Name}</h1></Card.Title>
+          <Card.Text class="genre-description"><b>Description:</b> {genre.Description}</Card.Text>
+          <Button onClick={() => onBackClick(null)} variant="primary">Back</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+GenreView.propTypes = {
+  genre: PropTypes.shape({
+    Description: PropTypes.string.isRequired
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
+};
