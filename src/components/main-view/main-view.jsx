a59fd7f9041853d9b2f079f728d8bec07cb1e1c2
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-28 01:12:39 -0400

    Cleaned up code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import axios from 'axios';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
-import { LoginView } from '../login-view/login-view';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 
-import './main-view.scss';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export class MainView extends React.Component {
 
