73fe7a40d70c2264e8afda5055edc288a6f55e2d
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-01 13:59:20 -0400

    Made MainView default

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MainView } from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
+import MainView from './components/main-view/main-view';
 
 import './index.scss';
 
@@ -17,8 +17,7 @@ class MyFlixApplication extends React.Component {
 }
 
 // Finds the root of the app
-const container = document.getElementsByClassName('app-container')
-[0];
+const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render the app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
