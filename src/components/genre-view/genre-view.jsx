d6fb96e6e4f561016a922822f77070292c09eefc
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-01 13:51:35 -0400

    Added block style to button

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -12,8 +12,8 @@ export class GenreView extends React.Component {
       <Card className='genre-view'>
         <Card.Body>
           <Card.Title><h1>{genre.Name}</h1></Card.Title>
-          <Button onClick={() => onBackClick(null)} variant="primary">Back</Button>
           <Card.Text className="genre-description"><b>Description:</b> {genre.Description}</Card.Text>
+          <Button onClick={() => onBackClick(null)} block variant="primary">Back</Button>
         </Card.Body>
       </Card>
     );
