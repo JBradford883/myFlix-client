a18beba264a4a06656ecc0b84f633ca42a06a708
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-01 22:58:17 -0400

    Updated registration form

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 export function RegistrationView(props) {
   const [username, setUserName] = useState('');
@@ -10,34 +11,51 @@ export function RegistrationView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password, email, birthday);
-    props.onRegister(username);
+    /* Send a request to the server for authentication */
+    axios.post('https://myflix-2388-app.herokuapp.com/users', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+
+    })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
+      })
+      .catch(e => {
+        console.log('error registering the user')
+      });
   };
 
   // Form to register username, password, email, birthday
   return (
-    <Form className="col-md-6 offset-3">
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" placeholder="Enter Username" onChange={e => setUserName(e.target.value)} />
-      </Form.Group>
+    <>
+      <h1 className="headline text-center font-weight-bold text-danger">Register for myFlix App</h1>
+      <Form className="col-md-6 offset-3">
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type="text" placeholder="Enter Username" onChange={e => setUserName(e.target.value)} />
+        </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
 
-      <Form.Group controlId="formEmail">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control type="text" placeholder="name@example.com" onChange={e => setEmail(e.target.value)} />
-      </Form.Group>
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control type="text" placeholder="name@example.com" onChange={e => setEmail(e.target.value)} />
+        </Form.Group>
 
-      <Form.Group controlId="formBirthday">
-        <Form.Label>Birthday:</Form.Label>
-        <Form.Control type="text" placeholder="dd/mm/yyyy" value={birthday} onChange={e => setBirthday(e.target.value)} />
-      </Form.Group>
+        <Form.Group controlId="formBirthday">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control type="text" placeholder="dd/mm/yyyy" value={birthday} onChange={e => setBirthday(e.target.value)} />
+        </Form.Group>
 
-      <Button variant="danger" type="submit" onClick={handleSubmit}>Submit</Button>
-    </Form>
+        <Button variant="danger" type="submit" onClick={handleSubmit}>Submit</Button>
+      </Form>
+    </>
   );
 }
