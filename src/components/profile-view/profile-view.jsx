db3dd8c9648da345b20b44de176b8aa582409f46
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-14 18:56:02 -0400

    Added in logic to updated user profile

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -5,42 +5,74 @@ import './profile-view.scss';
 
 export class ProfileView extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValues: {
+        Username: props.userData.Username,
+        Password: '',
+        Email: props.userData.Email,
+        Birthday: props.userData.Birthday
+      },
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const { target } = e;
+    this.setState((prev) => ({
+      formValues: {
+        ...prev.formValues,
+        [target.name]: target.value,
+      },
+    }));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.updateProfile(this.state.inputValues)
+  }
+
 
   render() {
-    let { onBackClick, user, userData, token } = this.props;
+    let { userData, token, deleteAcc, handleSubmit } = this.props;
 
     return (
       <Container className="profile-view" >
         <Card>
           <h2 className="profile-title d-flex justify-content-center text-danger mt-2">Your Profile Info</h2>
           <Card.Body>
-            <Card.Title className="d-flex justify-content-center mb-1">Username: {`${userData.Username}`}</Card.Title>
-            <Card.Title className="d-flex justify-content-center mb-1">Email: {`${userData.Email}`}</Card.Title>
-            <Card.Title className="d-flex justify-content-center">Birthday: {`${userData.Birthday}`}</Card.Title>
+            <Card.Title className="d-flex justify-content-center mb-1">Username: {`${userData.Username} `}</Card.Title>
+            <Card.Title className="d-flex justify-content-center mb-1">Email: {`${userData.Email} `}</Card.Title>
+            <Card.Title className="d-flex justify-content-center mb-1">Birthday: {`${userData.Birthday} `}</Card.Title>
           </Card.Body>
         </Card>
 
+
         <Form className="block">
           <h2 className="profile-title d-flex justify-content-center text-danger mt-3">Update your profile</h2>
-          <Form.Group controlId="formUsername" onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formUsername">
             <Form.Label>Username</Form.Label>
             <Form.Control name="Username" type="username" placeholder="Update your username" onChange={this.handleChange} />
           </Form.Group>
-          <Form.Group controlId="formPassword" onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control name="Password" type="password" onChange={this.handleChange} />
           </Form.Group>
-          <Form.Group controlId="formEmail" onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formEmail">
             <Form.Label>Email</Form.Label>
             <Form.Control name="Email" type="email" onChange={this.handleChange} />
           </Form.Group>
-          <Form.Group controlId="formBirthday" onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formBirthday">
             <Form.Label>Birthday</Form.Label>
             <Form.Control name="Birthday" type="date" onChange={this.handleChange} />
           </Form.Group>
         </Form>
 
-        <Button block variant="danger" type="submit" onClick={() => { updateInfo(token) }}>Update Profile</Button>
+
+
+        <Button block variant="danger" type="submit" onClick={() => { handleSubmit(e) }}>Update Profile</Button>
 
         <Button variant="danger" block type="button" onClick={() => { deleteAcc(token); onSignOut(null); history.push('/'); }}>Delete My Account</Button>
 
@@ -48,3 +80,7 @@ export class ProfileView extends React.Component {
     );
   }
 }
+
+
+
+
