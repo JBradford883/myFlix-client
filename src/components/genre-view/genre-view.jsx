0ca857ec5e97f549395eb0599c6402d93cb9e024
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-21 16:00:07 -0400

    Added view for movies within the genre

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 
 import './genre-view.scss';
 
 export class GenreView extends React.Component {
 
   render() {
-    const { genre, onBackClick } = this.props;
+    const { genre, onBackClick, movies } = this.props;
 
     return (
-      <Card className='genre-view'>
-        <Card.Body>
-          <Card.Title><h1 className="font-weight-bold">{genre.Name}</h1></Card.Title>
-          <Card.Text className="genre-description"><b>Description:</b> {genre.Description}</Card.Text>
-          <Button onClick={() => onBackClick(null)} block variant="danger">Back</Button>
-        </Card.Body>
-      </Card>
+      <>
+        <Container>
+          <Card className='genre-view'>
+            <Card.Body>
+              <Card.Title><h1 className="font-weight-bold">{genre.Name}</h1></Card.Title>
+              <Card.Text className="genre-description"><b>Description:</b> {genre.Description}</Card.Text>
+              <Button onClick={() => onBackClick(null)} block variant="danger">Back</Button>
+            </Card.Body>
+          </Card>
+        </Container>
+
+        <Container className='my-3'>
+          <div>
+            <h4 className=' text-center mb-4 white-words'>{genre.Name} Movies:</h4>
+            <div className="genre-movies text-center">
+            </div>
+          </div>
+        </Container>
+
+      </>
     );
   }
 }
