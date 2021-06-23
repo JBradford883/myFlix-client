63f094918fb236d167d6a3cd1094ef9d513a30c0
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-23 15:38:21 -0400

    Added axios post request to add favorite movie

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,45 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Button, Container, Row } from 'react-bootstrap';
+import { Card, Button, Container, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 import { Link } from 'react-router-dom';
 
 import './movie-view.scss';
 
 export class MovieView extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  addFavorite(movie) {
+    let token = localStorage.getItem('token');
+    let user = localStorage.getItem('user');
+    axios.post(`https://myflix-2388-app.herokuapp.com/users/${user}/movies/${movie._id}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    }).then(
+      (response) => {
+        console.log(response);
+        alert('You have sucessfully updated your favorites list.');
+      }).catch(
+        function (error) {
+          console.log(error)
+          alert('There was an error.');
+        }
+      );
+  }
 
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
-      <Card className="movie-view shadow">
-        <div className="m-3">
-          <div className="movie-poster float-left mr-3">
-            <img src={movie.ImagePath} />
+      <Container>
+        <Card className="movie-view shadow">
+          <div className="m-4">
+            <div className="movie-poster float-left text-center mr-3">
+              <img src={movie.ImagePath} />
+            </div>
+            <div className="movie-title text-center mb-3">
+              <span className="value font-weight-bold h3">{movie.Title}</span>
+            </div>
+            <div className="movie-info mb-2">
+              <span className="label font-weight-bold">Description: </span>
+              <span className="value">{movie.Description}</span>
+            </div>
+            <div className="movie-description mb-2">
+              <span className="label font-weight-bold">Director: </span>
+              <Link className="link" to={`/directors/${movie.Director.Name}`}>
+                <span className="value">{movie.Director.Name}</span>
+              </Link>
+            </div>
+            <div className="movie-description mb-2">
+              <span className="label font-weight-bold">Genre: </span>
+              <Link className="link" to={`/genres/${movie.Genre.Name}`}>
+                <span className="value">{movie.Genre.Name}</span>
+              </Link>
+            </div>
+            <div className="">
+              <Button className='add-favorite mt-3' variant='danger' onClick={() => this.addFavorite(movie)}>Add to Favorites</Button>
+            </div><div>
+              <Link to={`/`}>
+                <Button className="return-button mt-2" variant="danger">Back to Main</Button>
+              </Link>
+            </div>
           </div>
-          <div className="movie-title mb-2">
-            <span className="value font-weight-bold h2">{movie.Title}</span>
-          </div>
-          <div className="movie-info mb-2">
-            <span className="label font-weight-bold">Description: </span>
-            <span className="value">{movie.Description}</span>
-          </div>
-          <div className="movie-description mb-2">
-            <span className="label font-weight-bold">Director: </span>
-            <Link className="link" to={`/directors/${movie.Director.Name}`}>
-              <span className="value">{movie.Director.Name}</span>
-            </Link>
-          </div>
-          <div className="movie-description mb-2">
-            <span className="label font-weight-bold">Genre: </span>
-            <Link className="link" to={`/genres/${movie.Genre.Name}`}>
-              <span className="value">{movie.Genre.Name}</span>
-            </Link>
-          </div>
-          {/*<Button onClick={() => onBackClick()} variant='dark'>Back</Button>*/}
-
-        </div>
-      </Card>
+        </Card>
+        {/* <Button className="mt-3" onClick={() => onBackClick()} block variant='dark'>Back</Button> */}
+      </Container>
     );
   }
 }
