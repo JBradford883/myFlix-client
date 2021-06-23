457e1fb9bad6e74fe37c6999dfb5db2668006696
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-23 15:38:04 -0400

    Removed axios post request for favorite movies

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -8,25 +8,6 @@ import './movie-card.scss';
 import axios from 'axios';
 
 export class MovieCard extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  // POST request to add movie to users favorites list
-  addFavorite(movieData) {
-    axios.post(`https://myflix-2388-app.herokuapp.com/users/${this.props.user}/movies/${movie._id}`,
-    )
-      .then(response => {
-        console.log(response);
-        console.log('Successfully added movies to Favorites list');
-        alert("Added to favorites!")
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
 
   render() {
     const { movie } = this.props;
@@ -40,9 +21,12 @@ export class MovieCard extends React.Component {
           <Link to={`/movies/${movie._id}`}>
             <Button className="mb-2" block variant="danger">Movie Info</Button>
           </Link>
-          <div>
-            <Button className="Favorite-Movies" block variant="danger" onClick={() => this.addFavorite(movie._id)}>Add to Favorites</Button>
-          </div>
+          <Link to={`/directors/${movie.Director.Name}`}>
+            <Button className="mb-2" block variant="danger">Director</Button>
+          </Link>
+          <Link to={`/genres/${movie.Genre.Name}`}>
+            <Button className="mb-2" block variant="danger">Genre</Button>
+          </Link>
         </Card.Body>
       </Card >
     );
