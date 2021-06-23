b72dd3719b747168f435828f9b0439c0df313581
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-23 15:37:17 -0400

    updating genre card styling

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 
+import { Link } from "react-router-dom";
+
 import './genre-view.scss';
 
 export class GenreView extends React.Component {
@@ -11,25 +13,27 @@ export class GenreView extends React.Component {
 
     return (
       <Container>
-        <Card className='genre-view'>
+        <Card className='genre-view shadow'>
           <Card.Body>
-            <Card.Title className="font-weight-bold text-center h1">{genre.Name}</Card.Title>
+            <h1 className="font-weight-bold text-center">{genre.Name}</h1>
             <Card.Text className="genre-description"><b>Description:</b> {genre.Description}</Card.Text>
-            <Card.Text cclassName="text-center h5 mb-4">Movies belonging to this genre:</Card.Text>
+            <Card.Text className="text-center h5 mb-4">Movies belonging to this genre:</Card.Text>
 
             <Row className="genre-movies mb-3">
               {movies.map(m => {
                 if (m.Genre && m.Genre.Name === genre.Name) {
                   return (
                     <Col className="genre-card text-center mb-2" lg={3} md={6}>
-                      <Card.Img className="genre-img" key={m._id} src={m.ImagePath} />
+                      <Link to={`/movies/${m._id}`}>
+                        <Card.Img className="genre-img" key={m._id} src={m.ImagePath} />
+                      </Link>
                     </Col>
                   );
                 }
               })}
             </Row>
 
-            <Button onClick={() => onBackClick(null)} variant="danger">Back</Button>
+            <Button onClick={() => onBackClick(null)} variant="dark">Back</Button>
           </Card.Body>
         </Card>
       </Container>
