490384558faed7ca03bae380d2655f1f845f90db
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-27 00:32:08 -0400

    Updated favorite movies display

--- src/components/favorite-movies/favorite-movies.jsx
+++ src/components/favorite-movies/favorite-movies.jsx
@@ -10,7 +10,10 @@ export class FavoriteMovies extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.removeFavorite = this.removeFavorite.bind(this);
+  }
+
+  componentDidUpdate() {
+    return true;
   }
 
   // DELETE request to remove a movie from favorites list
@@ -32,26 +35,31 @@ export class FavoriteMovies extends React.Component {
   }
 
   render() {
-    const FavoriteMovies = this.props.user.FavoriteMovies;
+    const favoriteMovies = this.props.userData.FavoriteMovies;
     const { movies } = this.props;
 
     return (
       <Container>
         <Card className='fav-view shadow'>
           <Card.Body>
-            <Row className="fav-movies mb-3">
-              {FavoriteMovies.length === 0 && <div className='card-content'>You don't have any favorite movies yet!</div>}
+            <h2 className="profile-title d-flex justify-content-center text-danger">Your Favorite Movies</h2>
+            <Row className="fav-movies mb-3 justify-content-center">
+              {favoriteMovies.length === 0 && <div className='card-content'>You don't have any favorite movies yet!</div>}
               <div className='favorites-container'>
-                {FavoriteMovies.length > 0 && movies.map((movie) => {
-                  if (movie._id === FavoriteMovies.find((favMovie) => favMovie === movie._id)) {
-                    return (
-                      <Col className="fav-card text-center mb-2" lg={3} md={6}>
-                        <Link to={`/movies/${m._id}`}>
-                          <Card.Img className="fav-img" key={movie._id} src={movie.ImagePath} />
-                        </Link>
-                      </Col>
-                    );
-                  }
+                {favoriteMovies.length > 0 && movies.filter(movie => {
+                  return movie._id === favoriteMovies.find((favMovie) => favMovie === movie._id)
+                }).map((movie) => {
+                  return (
+                    <Col className="fav-card text-center mb-2" key={movie._id}>
+                      <Link to={`/movies/${movie._id}`}>
+                        <Card.Img className="fav-img" key={movie._id} src={movie.ImagePath} />
+                      </Link>
+                      <Card.Body className='movie-card-body'>
+                        <Button className='remove-favorite' variant='danger' onClick={() => this.removeFavorite(movie)}> Remove
+                        </Button>
+                      </Card.Body>
+                    </Col>
+                  );
                 })}
               </div>
             </Row>
