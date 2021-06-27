6ddbb7e704fcec20c481664fe89cb676284e9dce
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-27 00:30:29 -0400

    Imported axios

--- src/components/favorite-movies/favorite-movies.jsx
+++ src/components/favorite-movies/favorite-movies.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
 import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 
 import { Link } from "react-router-dom";
