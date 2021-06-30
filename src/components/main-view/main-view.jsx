589cbf8f50ed797e44272a77920eb5d371ebe918
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-30 10:26:00 -0400

    imported connect from react-redux

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import { connect } from 'react-redux';
+
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import { LoginView } from '../login-view/login-view';
